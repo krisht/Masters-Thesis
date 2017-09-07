0c174e34b9b5de77b2f434224297dd0ca2015600
Krishna <krisht@cooper.edu>
2017-09-07 00:13:40 -0400

    Added inception_v3

--- src/BrainNet.py
+++ src/BrainNet.py
@@ -313,6 +313,7 @@ class BrainNet:
 
 	def inception_v3(self, inputs, dropout_keep_prob=0.8, reuse=False, scope=''):
 		end_points = {}
+		inputs = tf.expand_dimis(inputs, dim=3)
 		with tf.name_scope(scope, 'inception_v3', [inputs]):
 			with slim.arg_scope([slim.layers.conv2d, slim.layers.fully_connected, slim.layers.batch_norm, slim.layers.dropout], weights_initializer=tf.contrib.layers.xavier_initializer(uniform=True), weights_regularizer=slim.l2_regularizer(self.l2_weight), reuse=reuse):
 				with slim.arg_scope([slim.layers.conv2d, slim.layers.max_pool2d, slim.layers.avg_pool2d], stride=1, padding='VALID'):
