3a898e3b07b962a6bf87cd5327dcde463bef2919
Krishna Thiyagarajan <krisht@cooper.edu>
2017-09-14 11:03:04 -0400

    Added all

--- src/BrainNet.py
+++ src/BrainNet.py
@@ -595,7 +595,8 @@ class BrainNet:
 		self.sess.close()
 		return epoch, val_percentage, val_conf_matrix
 
-	def get_sample(self, size=1, prob_selection=0.5, validation=False):
+	def get_sample(self, size=1, prob_selection=0.1, validation=False):
+		print("Getting sample")
 		data_list = []
 		class_list = []
 
