9174afa85e476b8011d2fceeb78d7b73e3b6411d
Krishna Thiyagarajan <krisht@cooper.edu>
2018-02-12 14:55:46 -0500

    Added alL

--- src/BrainNet.py
+++ src/BrainNet.py
@@ -745,57 +745,55 @@ class BrainNet:
 
 		# Files with Seizures
 		
-		val_inputs, val_classes = self.get_sample(size=self.validation_size, validation=True, with_seizure=True)
-		vector_val_inputs = self.sess.run(self.inference_model, feed_dict = {self.inference_model: val_inputs})
-		del val_inputs
+		val_inputs_seizure, val_classes_seizure = self.get_sample(size=self.validation_size)
+		vector_val_inputs_seizure = self.sess.run(self.inference_model, feed_dict={self.inference_input: val_inputs_seizure})
+		del val_inputs_seizure
 
+		pred_class_seizure = tempClassifier.predict(vector_val_inputs_seizure)
 
-		pred_class = tempClassifier.predict(vector_val_inputs)
-
-		percentage = len([i for i, j in zip(val_classes, pred_class) if i == j]) * 100.0 / self.validation_size
+		percentage_seizure = len([i for i, j in zip(val_classes_seizure, pred_class_seizure) if i == j]) * 100.0 / self.validation_size
 
 		if self.DEBUG:
-			print("Seizure File Validation Results: %.3f%% of of %d correct" % (percentage, self.validation_size))
+			print("Validation Results: %.3f%% of of %d correct" % (percentage_seizure, self.validation_size))
 
-		val_classes = list(map(lambda x: self.num_to_class[x], val_classes))
-		pred_class = list(map(lambda x: self.num_to_class[x], pred_class))
-		class_labels = [0, 1, 2, 3, 4, 5]
-		class_labels = list(map(lambda x: self.num_to_class[x], class_labels))
-		conf_matrix = confusion_matrix(val_classes, pred_class, labels=class_labels)
+		val_classes_seizure = list(map(lambda x: self.num_to_class[x], val_classes_seizure))
+		pred_class_seizure = list(map(lambda x: self.num_to_class[x], pred_class_seizure))
+		class_labels_seizure = [0, 1, 2, 3, 4, 5]
+		class_labels_seizure = list(map(lambda x: self.num_to_class[x], class_labels_seizure))
+		conf_matrix_seizure = confusion_matrix(val_classes_seizure, pred_class_seizure, labels=class_labels_seizure)
 		np.set_printoptions(precision=2)
 
-		np.save('./%s Results/%s_confusion_matrix_with_seizure_epoch%s_%.3f%%' % (curr_time, curr_time, epoch, percentage), conf_matrix)
+		np.save('./%s Results/%s_confusion_matrix_epoch%s_%.3f%%' % (curr_time, curr_time, epoch, percentage_seizure), conf_matrix_seizure)
 
-		plot_confusion_matrix(conf_matrix, classes=class_labels, epoch=epoch, accuracy=percentage, with_seizure=True, title="Confusion Matrix on Files with Seizures")
+		plot_confusion_matrix(conf_matrix_seizure, classes=class_labels_seizure, epoch=epoch, accuracy=percentage_seizure, with_seizure=True, title = "Confusion Matrix on Files with Seizure")
 
-		compute_tSNE(vector_val_inputs, classes, epoch=epoch, accuracy=percentage, num_to_label=self.num_to_class, title="t-SNE Embedding of DCNN Clustering Network on Files with Seizures")
+		#compute_tSNE(vector_inputs, classes, epoch=epoch, accuracy=percentage_seizure, num_to_label=self.num_to_class)
 
 		# Files without Seizures
 		
-		val_inputs, val_classes = self.get_sample(size=self.validation_size, validation=True, with_seizure=False)
-		vector_val_inputs = self.sess.run(self.inference_model, feed_dict = {self.inference_model: val_inputs})
-		del val_inputs
+		val_inputs_without_seizure, val_classes_without_seizure = self.get_sample(size=self.validation_size)
+		vector_val_inputs_without_seizure = self.sess.run(self.inference_model, feed_dict={self.inference_input: val_inputs_without_seizure})
+		del val_inputs_without_seizure
 
+		pred_class_without_seizure = tempClassifier.predict(vector_val_inputs_without_seizure)
 
-		pred_class = tempClassifier.predict(vector_val_inputs)
-
-		percentage = len([i for i, j in zip(val_classes, pred_class) if i == j]) * 100.0 / self.validation_size
+		percentage_without_seizure = len([i for i, j in zip(val_classes_without_seizure, pred_class_without_seizure) if i == j]) * 100.0 / self.validation_size
 
 		if self.DEBUG:
-			print("Seizure File Validation Results: %.3f%% of of %d correct" % (percentage, self.validation_size))
+			print("Validation Results: %.3f%% of of %d correct" % (percentage_without_seizure, self.validation_size))
 
-		val_classes = list(map(lambda x: self.num_to_class[x], val_classes))
-		pred_class = list(map(lambda x: self.num_to_class[x], pred_class))
-		class_labels = [0, 1, 2, 3, 4, 5]
-		class_labels = list(map(lambda x: self.num_to_class[x], class_labels))
-		conf_matrix = confusion_matrix(val_classes, pred_class, labels=class_labels)
+		val_classes_without_seizure = list(map(lambda x: self.num_to_class[x], val_classes_without_seizure))
+		pred_class_without_seizure = list(map(lambda x: self.num_to_class[x], pred_class_without_seizure))
+		class_labels_without_seizure = [0, 1, 2, 3, 4, 5]
+		class_labels_without_seizure = list(map(lambda x: self.num_to_class[x], class_labels_without_seizure))
+		conf_matrix_without_seizure = confusion_matrix(val_classes_without_seizure, pred_class_without_seizure, labels=class_labels_without_seizure)
 		np.set_printoptions(precision=2)
 
-		np.save('./%s Results/%s_confusion_matrix_without_seizure_epoch%s_%.3f%%' % (curr_time, curr_time, epoch, percentage), conf_matrix)
+		np.save('./%s Results/%s_confusion_matrix_epoch%s_%.3f%%' % (curr_time, curr_time, epoch, percentage_without_seizure), conf_matrix_without_seizure, with_seizure=False, title = "Confusion Matrix on Files without Seizure")
 
-		plot_confusion_matrix(conf_matrix, classes=class_labels, epoch=epoch, accuracy=percentage, with_seizure=False, title="Confusion Matrix on Files without Seizures")
+		plot_confusion_matrix(conf_matrix_without_seizure, classes=class_labels_without_seizure, epoch=epoch, accuracy=percentage_without_seizure)
 
-		compute_tSNE(vector_val_inputs, classes, epoch=epoch, accuracy=percentage, num_to_label=self.num_to_class, title="t-SNE Embedding of DCNN Clustering Network on Files without Seizures")
+		#compute_tSNE(vector_inputs, classes, epoch=epoch, accuracy=percentage_without_seizure, num_to_label=self.num_to_class)
 
 		self.count_of_triplets = dict()
 
